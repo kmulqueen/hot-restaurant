57755dfec0a03e13b775919a9d838ec31c698412
Olga <oserenchenko@gmail.com>
2018-08-14 19:21:36 -0600

    set up routes

--- server.js
+++ server.js
@@ -9,24 +9,36 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var reservations = [];
+var waitList = [];
+
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
+app.get("/api/reservations", function (req, res) {
+  return res.json(reservations);
+});
 
-app.listen(PORT, function() {
+app.get("/api/waitlist", function (req, res) {
+  return res.json(waitList);
+});
+
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
